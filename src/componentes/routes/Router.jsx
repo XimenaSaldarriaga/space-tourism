3f7b172e7c2a6ae565841aaba36c012c7b96681e
Mafer <vegapachecofer@gmail.com>
2023-07-30 15:47:36 -0500

    actualizacion

--- src/componentes/routes/Router.jsx
+++ src/componentes/routes/Router.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Home from '../home/Home'
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from '../home/Home.jsx';
+import Destination from "../components/destination/Destination";
+import Crew from "../components/crew/Crew";
+import Technology from "../components/technology/Technology";
+
 
 const Router = () => {
   return (
     <BrowserRouter>
     <Routes>
     <Route path='/' element={<Home />}/>
+    <Route path="/destination" element={<Destination />} />
+        <Route path="/crew" element={<Crew />} />
+        <Route path="/technology" element={<Technology />} />
     </Routes>
     </BrowserRouter>
   )
--- src/componentes/space/Space.jsx
+++ src/componentes/space/Space.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './space.scss'
 
 const Space = () => {
   return (
@@ -7,7 +6,7 @@ const Space = () => {
         <div className='space__text'>
             <span className='space__travel'>SO, YOU WANT TO TRAVEL TO</span>
             <span className='space__title'>SPACE</span>
-            <span className='space__lets'>Let's face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</span>
+            <span className='space__lets'>Let´s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we'll give you a truly out of this world experience!</span>
         </div>
         <div className='space__explore'>
             <a>EXPLORE</a>
