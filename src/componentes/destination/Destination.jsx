b96bca98e9784c63a49cded03c27b58da4c84b6b
Mafer <vegapachecofer@gmail.com>
2023-07-30 19:16:12 -0500

    mafer css

--- src/componentes/destination/Destination.jsx
+++ src/componentes/destination/Destination.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import data from "../../../data.json";
+import "./style.scss"
 
 export const Destination = () => {
   const [planets] = useState(data.destinations);
